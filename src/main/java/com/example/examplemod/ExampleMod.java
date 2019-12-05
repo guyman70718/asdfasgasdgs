0039964ddea3a42606b9b297fe475f1f6bf4c049
guyman70718 <guyman70718@gmail.com>
2019-12-04 22:32:50 -0500

    pp

--- src/main/java/com/example/examplemod/ExampleMod.java
+++ src/main/java/com/example/examplemod/ExampleMod.java
@@ -3,13 +3,17 @@ package com.example.examplemod;
 import com.example.examplemod.events.Events;
 import edu.wpi.first.networktables.NetworkTableInstance;
 
+import edu.wpi.first.networktables.PersistentException;
 import net.minecraft.init.Blocks;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.Mod.EventHandler;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+import edu.wpi.first.networktables.NetworkTableEntry;
 import org.apache.logging.log4j.Logger;
+
+import java.util.function.Consumer;
 
 @Mod(modid = ExampleMod.MODID, name = ExampleMod.NAME, version = ExampleMod.VERSION)
 public class ExampleMod
@@ -18,6 +22,8 @@ public class ExampleMod
     public static final String NAME = "Frc Control";
     public static final String VERSION = "1.0";
 
+    NetworkTableEntry yaw;
+    public static double setpoint = 0;
     private static Logger logger;
     public static NetworkTableInstance table;
     @EventHandler
@@ -31,6 +37,13 @@ public class ExampleMod
         table = NetworkTableInstance.create();
         table.setServerTeam(2869);
         table.startServer();
+        try {
+            table.loadEntries("omar.txt", "");
+        } catch (PersistentException e) {
+            e.printStackTrace();
+        }
+        //table.addEntryListener("hello", null, 5);
+
         MinecraftForge.EVENT_BUS.register(new Events());
     }
 }
--- src/main/java/com/example/examplemod/events/Events.java
+++ src/main/java/com/example/examplemod/events/Events.java
@@ -1,13 +1,41 @@
 package com.example.examplemod.events;
 
+import com.example.examplemod.ExampleMod;
+import edu.wpi.first.networktables.NetworkTableEntry;
+import edu.wpi.first.networktables.PersistentException;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.entity.EntityPlayerSP;
+import net.minecraft.client.settings.KeyBinding;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.util.math.MathHelper;
+import net.minecraft.util.math.Vec3d;
+import net.minecraftforge.event.entity.EntityJoinWorldEvent;
+import net.minecraftforge.event.entity.living.LivingSpawnEvent;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+import net.minecraftforge.fml.common.gameevent.PlayerEvent;
 import net.minecraftforge.fml.common.gameevent.TickEvent;
+import net.minecraftforge.fml.common.gameevent.TickEvent.PlayerTickEvent;
+import net.minecraftforge.fml.common.gameevent.InputEvent.KeyInputEvent;
+
+import net.minecraftforge.fml.common.network.FMLNetworkEvent;
 
 public class Events {
-
+    @SubscribeEvent
+    public void onPlayerJoin(EntityJoinWorldEvent  event){
+        if(event.getEntity() instanceof EntityPlayer) {
+            event.getEntity().rotationYaw = 0.0f;
+        }
+    }
 
     @SubscribeEvent
-    public void onTick(TickEvent.ClientTickEvent event){
-        System.out.println("tick");
+    public void onTick(TickEvent.PlayerTickEvent event) {
+        if(Minecraft.getMinecraft().isIntegratedServerRunning() ){
+            try {
+                ExampleMod.table.getEntry("yaw").setDouble(Minecraft.getMinecraft().player.rotationYaw);
+                ExampleMod.table.getEntry("pitch").setDouble(Minecraft.getMinecraft().player.rotationPitch);
+            }catch(Exception e){
+                System.out.println("not in game yet");
+            }
+        }
     }
 }
